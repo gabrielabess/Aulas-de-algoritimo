8fbf8abfc854675a8488ba2974c8886c004a4be9
Gabriela Bés <gabriela.ifrs.ads@gmail.com>
2025-04-03 20:02:56 -0300

    Create 03-04 questao 2.c

---
+++ 03-04 questao 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	int ano_nasc, ano_atual=2025, idade;
+	printf("Insira seu ano de nascimento:");
+	scanf("%d", &ano_nasc);
+	idade=ano_atual-ano_nasc;
+	
+	if(idade>=16 && idade<18) {
+		printf("Pode votar mas e opcional!");
+}
+	else if(idade>=18 && idade<65){
+		printf("Pode votar e e obrigatorio");
+	}
+	else if(idade>=65){
+		printf("Pode votar mas nao e obrigatorio");
+	}
+	else{
+		printf("Ainda nao pode votar!");
+	}
+}
